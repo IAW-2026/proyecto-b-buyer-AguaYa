40fca808380c4eac8f23309faf58366744dbd6ec
Leon Alvarez <elionalvino@gmail.com>
2026-05-21 20:55:01 -0300

    se agrega primera version de paginas de signin y sign up

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,10 +29,14 @@ export default function RootLayout({
         <ClerkProvider>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <Show when="signed-out">
-              <SignInButton />
+              <SignInButton >
+                <button  className="bg-[#4287f5] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Iniciar Sesion
+                </button>
+              </SignInButton>
               <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
+                <button className="bg-[#4287f5] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Registrarse
                 </button>
               </SignUpButton>
             </Show>
--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,26 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+/*import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
   // exclude _next, static files and the Clerk auth routes (signin/callbacks)
   matcher: ["/((?!.*\\..*|_next|signin).*)"],
+};*/
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/signin(.*)",
+  "/signup(.*)",
+  "/"
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)"], 
 };
