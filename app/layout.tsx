15ecbc45644b653828d6942e44ab38018f9e04fb
Leon Alvarez <elionalvino@gmail.com>
2026-05-26 00:13:43 -0300

    se altera el frontend de la pagina inicial

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -27,22 +26,30 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ClerkProvider>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <Show when="signed-out">
-              <SignInButton >
-                <button  className="bg-[#4287f5] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Iniciar Sesion
-                </button>
-              </SignInButton>
-              <SignUpButton>
-                <button className="bg-[#4287f5] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Registrarse
-                </button>
-              </SignUpButton>
-            </Show>
-            <Show when="signed-in">
-              <UserButton />
-            </Show>
+          <header className="flex justify-between items-center p-4 gap-4 h-16 bg-white shadow-sm">
+            {/* Logo - Izquierda */}
+            <div className="flex-shrink-0">
+              <h1 className="text-2xl font-bold text-[#4287f5]">AguaYa</h1>
+            </div>
+
+            {/* Botones - Derecha */}
+            <div className="flex items-center gap-4 flex-shrink-0">
+              <Show when="signed-out">
+                <SignInButton >
+                  <button  className="bg-[#4287f5] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Iniciar Sesion
+                  </button>
+                </SignInButton>
+                <SignUpButton>
+                  <button className="bg-[#4287f5] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Registrarse
+                  </button>
+                </SignUpButton>
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
+            </div>
           </header>
           {children}
         </ClerkProvider>
