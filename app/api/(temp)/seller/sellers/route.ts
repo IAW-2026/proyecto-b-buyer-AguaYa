bfb0aee75fb611a8b745b52da02593007c3d51ee
Leon Alvarez <elionalvino@gmail.com>
2026-05-28 01:01:30 -0300

    se agrega endpoint mock para vendors

---
+++ app/api/(temp)/seller/sellers/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse,NextRequest } from "next/server";
+
+/*model Vendor {
+  id          String    @id @default(cuid())
+  userId      String    @unique
+  name        String
+  description String?
+  address     String
+  image       String?
+  cuil        String?
+  cuit        String?
+  reputation  Float     @default(0)
+  createdAt   DateTime  @default(now())
+  updatedAt   DateTime  @updatedAt
+  deletedAt   DateTime? // Soft delete: vendedor desactivado, datos preservados
+
+  products    Product[]
+  orders      Order[]
+}*/
+
+const mockVendors = [
+    {id:"1", name:"Vendedor1",address:"Buenos Aires"},
+    {id:"2", name:"Vendedor2",address:"Bahia Blanca"},
+    {id:"3", name:"Vendedor3",address:"Buenos Aires"},
+    {id:"4", name:"Vendedor4",address:"Punta Alta"},
+    {id:"5", name:"Vendedor5",address:"Bahia Blanca"}
+]
+
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = request.nextUrl;
+    const id = searchParams.get('id');
+    const name = searchParams.get('name');
+    const address = searchParams.get('Address');
+    let result = [...mockVendors];
+    if (id) {
+        result = result.filter(p => p.id === id)
+    }
+    if (name) {
+        result = result.filter(p => p.name == name)
+    }
+    if (address) {
+        result = result.filter(p => p.address == address)
+    }
+    return NextResponse.json;
+}
