fd1b90852d0a1c685cd8b4af9cd5dbbf595578ae
Leon Alvarez <elionalvino@gmail.com>
2026-05-30 05:45:22 -0300

    se modifican los mocks para permitir deploy

--- lib/external_api_calls/products.ts
+++ lib/external_api_calls/products.ts
@@ -1,11 +1,23 @@
+
+const mockProducts = [ 
+    {id:"1", vendorId:"1", name:"Producto1", price:100, stock:10},
+    {id:"2", vendorId:"1", name:"Producto2", price:200, stock:20},
+    {id:"3", vendorId:"2", name:"Producto3", price:300, stock:30},
+    {id:"4", vendorId:"2", name:"Producto4", price:400, stock:40},
+    {id:"5", vendorId:"3", name:"Producto5", price:500, stock:50}
+]
+
 export async function getProducts(vendorId?: string){
-    let baseUrl = process.env.NEXT_PUBLIC_API_URL;
+   /* let baseUrl = process.env.NEXT_PUBLIC_API_URL;
     let url= `${baseUrl}/seller/products`;
     if (vendorId) {
         url += `?vendor_id=${encodeURIComponent(vendorId)}`;
     }
     console.log(url);
     const res = await fetch(url);
-    if (!res.ok) throw new Error('Error al obtener productos');
-    return res.json();
+    if (!res.ok) throw new Error('Error al obtener productos');*/
+    if (vendorId) {
+        return mockProducts.filter(p => p.vendorId === vendorId);
+    }
+    return mockProducts;
 }
--- lib/external_api_calls/vendors.ts
+++ lib/external_api_calls/vendors.ts
@@ -1,17 +1,28 @@
 
-
+const mockVendors = [
+    {id:"1", name:"Vendedor1",address:"Buenos Aires"},
+    {id:"2", name:"Vendedor2",address:"Bahia Blanca"},
+    {id:"3", name:"Vendedor3",address:"Buenos Aires"},
+    {id:"4", name:"Vendedor4",address:"Punta Alta"},
+    {id:"5", name:"Vendedor5",address:"Bahia Blanca"}
+]
 
 
 
 export async function getVendors(id?: string){
 
-    let baseUrl = process.env.NEXT_PUBLIC_API_URL;
+   /* let baseUrl = process.env.NEXT_PUBLIC_API_URL;
     let url= `${baseUrl}/seller/sellers`;
     if (id) {
         url += `?id=${encodeURIComponent(id)}`;
     }
     const res = await fetch(url);
     console.log(process.env.NEXT_PUBLIC_API_URL);
-    if (!res.ok) throw new Error('Error al obtener vendedores');
-    return res.json();
+    if (!res.ok) throw new Error('Error al obtener vendedores');*/
+    if(id){
+        const vendor = mockVendors.find(v => v.id === id);
+        if (!vendor) throw new Error('Vendedor no encontrado');
+        return vendor;
+    }
+    return mockVendors;
 }
