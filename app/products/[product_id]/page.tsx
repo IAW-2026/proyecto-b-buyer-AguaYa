fda05e6dc691d3db66ebda3e807bab18d50514d8
Leon Alvarez <elionalvino@gmail.com>
2026-06-01 23:03:40 -0300

    se modifica product page

--- app/products/[product_id]/page.tsx
+++ app/products/[product_id]/page.tsx
@@ -1,46 +1,28 @@
-/*
 import React from "react";
-import{getProducts,getProductsByVendor,Product} from "@/lib/external_api_calls/products";
+import { getProductById } from "@/lib/external_api_calls/products"
 import ProductCard from "@/app/components/products/productCard";
+import Link from "next/link";
 
-type VendorPageProps = {
-  params: Promise<{ vendor_id: string }>;
-};
-
-export default async function VendorPage({ params }: VendorPageProps) {
-  const { vendor_id } = await params;
-  const products = await getProductsByVendor(vendor_id);
-  return (
-    <div>
-      <div>
-        <h1>Vendor ID: {vendor_id}</h1>
-      </div>
-      <div className="grid grid-cols-3 gap-4 p-4">
-        {products.map((product: Product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-    </div>
-  );
-}
-*/ 
-
-import React from "react";
-import {Product,getProductById} from "@/lib/external_api_calls/products"
-import { AddToCartButton } from "@/app/components/cart/addToCartButton";
 type ProductPageProps = {
   params: Promise<{ product_id: string }>;
 };
 
-export default async function ProductPage({params}:ProductPageProps){
-    const {product_id} = await params;
-    const product = await getProductById(product_id);
-    return (
-        <div>
-            <h1>{product.name}</h1>
-            <AddToCartButton productId={product.id}/>
+export default async function ProductPage({ params }: ProductPageProps) {
+  const { product_id } = await params;
+  const product = await getProductById(product_id);
+
+  return (
+    <div>
+      <nav className="bg-white border-b border-gray-200 shadow-sm">
+        <div className="flex divide-x divide-gray-200">
+          <Link href={`/`} className="flex-1 text-center py-3 text-sm font-medium text-gray-700 hover:bg-gray-50 transition-colors">
+            ← Volver
+          </Link>
         </div>
-    );
+      </nav>
+      <div className="max-w-sm mx-auto p-6">
+        <ProductCard product={product} />
+      </div>
+    </div>
+  );
 }
-
-
