eeee44180242eb535418df8f09141a51a15bb54b
Leon Alvarez <elionalvino@gmail.com>
2026-06-01 23:15:00 -0300

    se agregan mas productos y vendedores mock

--- lib/external_api_calls/vendors.ts
+++ lib/external_api_calls/vendors.ts
@@ -1,17 +1,22 @@
-
 export type Vendor = {
   id: string
   name: string
   address: string
 }
 
-
 const mockVendors: Vendor[] = [
-    {id:"1", name:"Vendedor1",address:"Buenos Aires"},
-    {id:"2", name:"Vendedor2",address:"Bahia Blanca"},
-    {id:"3", name:"Vendedor3",address:"Buenos Aires"},
-    {id:"4", name:"Vendedor4",address:"Punta Alta"},
-    {id:"5", name:"Vendedor5",address:"Bahia Blanca"}
+    {id:"1", name:"Vendedor1", address:"Buenos Aires"},
+    {id:"2", name:"Vendedor2", address:"Bahia Blanca"},
+    {id:"3", name:"Vendedor3", address:"Buenos Aires"},
+    {id:"4", name:"Vendedor4", address:"Punta Alta"},
+    {id:"5", name:"Vendedor5", address:"Bahia Blanca"},
+    {id:"6", name:"Vendedor6", address:"Mar del Plata"},
+    {id:"7", name:"Vendedor7", address:"Rosario"},
+    {id:"8", name:"Vendedor8", address:"Córdoba"},
+    {id:"9", name:"Vendedor9", address:"Mendoza"},
+    {id:"10", name:"Vendedor10", address:"Buenos Aires"},
+    {id:"11", name:"Vendedor11", address:"Punta Alta"},
+    {id:"12", name:"Vendedor12", address:"Bahia Blanca"},
 ]
 
 export async function getVendors(): Promise<Vendor[]> {
@@ -24,11 +29,3 @@ export async function getVendorById(id: string): Promise<Vendor | null> {
         return null;
     return vendor;
 }
- /* let baseUrl = process.env.NEXT_PUBLIC_API_URL;
-    let url= `${baseUrl}/seller/sellers`;
-    if (id) {
-        url += `?id=${encodeURIComponent(id)}`;
-    }
-    const res = await fetch(url);
-    console.log(process.env.NEXT_PUBLIC_API_URL);
-    if (!res.ok) throw new Error('Error al obtener vendedores');*/
