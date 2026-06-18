36eff7344baa11313d700faaf26fa2254b055e52
Leon Alvarez <elionalvino@gmail.com>
2026-06-18 01:07:40 -0300

    se utiliza mensaje de debug en el confirm payment

--- app/actions/payment.ts
+++ app/actions/payment.ts
@@ -3,5 +3,5 @@ import { setOrderStatus, OrderStatus } from "@/lib/orders";
 import { confirmPayment } from "@/lib/external_api_calls/payments";
 
 export async function confirmPaymentAction(orderId: string) {
-  await confirmPayment(orderId);
+  console.log(await confirmPayment(orderId));
 }
