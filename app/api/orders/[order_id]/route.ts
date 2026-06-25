7079e70661f740f14635adf4313912d1efed2bd2
Leon Alvarez <elionalvino@gmail.com>
2026-06-25 01:08:24 -0300

    se agrega endpoint get para order con su order id

--- app/api/orders/[order_id]/route.ts
+++ app/api/orders/[order_id]/route.ts
@@ -1,5 +1,22 @@
 import { NextRequest, NextResponse } from "next/server"
-import {OrderStatus,setOrderStatus} from "@/lib/orders"
+import {OrderStatus,setOrderStatus, getOrderById} from "@/lib/orders"
+
+export async function GET(request: NextRequest, { params }: { params: Promise<{ order_id: string }> }) {
+    const apiKey = request.headers.get("x-api-key");
+    if (apiKey !== process.env.BUYER_API_KEY) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    try {
+        const { order_id } = await params;
+        const order = await getOrderById(order_id);
+        if (!order) {
+            return NextResponse.json({ error: "Orden no encontrada" }, { status: 404 });
+        }
+        return NextResponse.json({ order }, { status: 200 });
+    } catch {
+        return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 });
+    }
+}
 
 export async function PATCH(request:NextRequest,{ params }:{ params: Promise<{ order_id: string }> }) {
     const body = await request.json();
